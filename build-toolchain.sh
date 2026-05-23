150f3c609c76351696a84f615aad32ba3a7d3d18
almostdead3 <128373638+almostdead3@users.noreply.github.com>
2026-05-23 15:45:19 +0530

    Update build-toolchain.sh

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -13,7 +13,7 @@ function msg() {
 # Build LLVM
 msg "Building LLVM..."
 ./build-llvm.py \
-	--branch "release/19.x" \
+	--branch "release/22.x" \
 	--clang-vendor "LLVM" \
 	--targets "ARM;AArch64;X86" \
 	"$repo_flag"
